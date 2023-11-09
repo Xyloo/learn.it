59b44cc655825ac46c2f70be8ba23ca98c529811
Xylo <cezary2000@gmail.com>
2023-11-09 22:47:11 +0100

    Add a JWT blacklist mechanism.
    The blacklist should clean itself up every 15 minutes and remove all expired tokens.

---
+++ learn.it/Utils/JwtSettings.cs
@@ -0,0 +1,33 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+
+namespace learn.it.Utils
+{
+    public class JwtSettings
+    {
+        public string Key { get; set; } = null!;
+    }
+
+    public static class InvalidJwtTokens
+    {
+        private static readonly Timer _timer = new(ClearInvalidTokens, null, TimeSpan.FromMinutes(5), TimeSpan.FromMinutes(15));
+        public static List<string> _invalidTokens = new();
+
+        public static void AddToken(string token)
+        {
+            _invalidTokens.Add(token);
+        }
+
+        private static void ClearInvalidTokens(object? state)
+        {
+            foreach (var token in _invalidTokens.Where(token => DateTime.UtcNow > new JwtSecurityToken(token).ValidTo))
+            {
+                _invalidTokens.Remove(token);
+            }
+        }
+
+        public static bool IsTokenInvalid(string token)
+        {
+            return _invalidTokens.Contains(token);
+        }
+    }
+}
