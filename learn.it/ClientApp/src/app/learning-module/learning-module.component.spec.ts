890ad9c06401008ac9462e239aadc47be8b394fa
svhp <tomasz.geszka@gmail.com>
2023-11-14 07:07:53 +0100

    WIP: add learning-module

---
+++ learn.it/ClientApp/src/app/learning-module/learning-module.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LearningModuleComponent } from './learning-module.component';
+
+describe('LearningModuleComponent', () => {
+  let component: LearningModuleComponent;
+  let fixture: ComponentFixture<LearningModuleComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LearningModuleComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(LearningModuleComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ learn.it/ClientApp/src/app/learning-module/learning-module.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-learning-module',
+  templateUrl: './learning-module.component.html',
+  styleUrls: ['./learning-module.component.css']
+})
+export class LearningModuleComponent {
+
+}
