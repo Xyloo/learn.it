ed9d61ede2882afc4786d4e4c49bbeabed38e529
svhp <tomasz.geszka@gmail.com>
2023-11-26 19:27:37 +0100

    WIP: Added home component:  - ui done  - some temp data for last activity and recommended

--- learn.it/ClientApp/src/app/home/home.component.ts
+++ learn.it/ClientApp/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivityDto } from '../models/lastactivity.dto';
 
 @Component({
   selector: 'app-home',
@@ -6,4 +7,33 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+
+
+  usersLastActivity: ActivityDto[] = [
+    {
+      id: 1,
+      name: "Nazwa zestawu 1",
+      avatarUrl: "/assets/plus-icon.svg",
+      username: "Userasdasd1",
+      isPrivate: true
+    },
+    {
+      id: 2,
+      name: "Nazwa zestawu 2",
+      avatarUrl: "/assets/plus-icon.svg",
+      username: "User2",
+      isPrivate: false
+    },
+    {
+      id: 3,
+      name: "Nazwa zestawu 2",
+      avatarUrl: "/assets/plus-icon.svg",
+      username: "User1",
+      isPrivate: true
+    }
+  ]
+
+
+
+
 }
